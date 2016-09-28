ce31a18f8cadc6a99a3d22512ceaa5ddb0098b79
billlipeng <billlipeng@gmail.com>
2016-09-28 14:12:10 -0500

    a script that download all data

---
+++ download.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_01.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_03.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_05.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_07.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_09.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2013_11.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_01.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_03.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_05.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_07.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_09.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2014_11.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_01.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_03.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_05.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_07.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_09.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2015_11.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_2016_01.xml.7z
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_meta.csv.7z      
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-16/wdvc16_truth.csv.7z
+wget http://groups.uni-paderborn.de/wdqa/tags_201501181252.csv.bz2
+wget http://www.uni-weimar.de/medien/webis/corpora/corpus-wdvc-15/wikidata-vandalism-corpus-2015.tar.bz2
