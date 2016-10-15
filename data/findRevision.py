cf4382fd3eecef60a07852d1b01e0deb3acc38f9
billlipeng <billlipeng@gmail.com>
2016-10-15 18:52:05 -0500

    init parser

--- data/findRevision.py
+++ data/findRevision.py
@@ -1,6 +1,6 @@
-with open('100_wdvc16_truth.csv') as f:
+with open('wdvc16_meta.csv') as f:
     line = f.readline()
     while len(line)!= 0:
-        if "2916961" == line.split(",")[0] or "2916994" == line.split(",")[0]:
+        if "2916961" == line.split(",")[0] or "2916994" == line.split(",")[3]:
             print line
         line = f.readline()
