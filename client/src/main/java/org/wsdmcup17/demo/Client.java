721d407170e81a4ff5e7f858a2c7d922245b87e5
onlinecco <x-joy@live.cn>
2016-12-01 00:02:52 -0600

    Client modified

--- client/src/main/java/org/wsdmcup17/demo/Client.java
+++ client/src/main/java/org/wsdmcup17/demo/Client.java
@@ -1,12 +1,18 @@
 package org.wsdmcup17.demo;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.io.RandomAccessFile;
 import java.io.Writer;
 import java.net.Socket;
 import java.net.URI;
@@ -23,6 +29,7 @@ import org.wikidata.wdtk.dumpfiles.DumpContentType;
 import org.wikidata.wdtk.dumpfiles.MwDumpFile;
 import org.wikidata.wdtk.dumpfiles.MwDumpFileProcessor;
 import org.wikidata.wdtk.dumpfiles.MwLocalDumpFile;
+import org.wikidata.wdtk.dumpfiles.MwRevision;
 import org.wikidata.wdtk.dumpfiles.MwRevisionDumpFileProcessor;
 import org.wikidata.wdtk.dumpfiles.MwRevisionProcessor;
 
@@ -102,11 +109,15 @@ public class Client {
 			BlockingQueue<CSVRecord> metadataQueue =
 					new ArrayBlockingQueue<>(QUEUE_CAPACITY);
 
+			File revisionFile = new File("~/data/revision");
+			OutputStream ofStream = new FileOutputStream(revisionFile);
+			File metadataFile = new File("~/data/metadata");
+			OutputStream mdStream = new FileOutputStream(revisionFile);
 			// Thread that demultiplexes the data stream from the server,
 			// writing revisions to the output stream and metadata to the queue.
 			Thread demultiplexerThread = createDemultiplexerThread(
-					dataStream, revisionOutputStream, metadataQueue);
-			
+					dataStream, ofStream, mdStream);
+			/*
 			// Event-driven revision processor based on Wikidata toolkit.
 			MwDumpFileProcessor revisionProcessor = createRevisionProcessor(
 					resultPrinter, metadataQueue);
@@ -115,12 +126,71 @@ public class Client {
 			// Note: the processor closes the stream and thus the socket.
 			revisionProcessor.processDumpFileContents(
 					revisionInputStream, createMwDumpFile());
-			
+			*/
+
+			classifyRevision(resultPrinter);
 			// Wait for the demultiplexer thread to terminate.
 			demultiplexerThread.join();
 		}
 	}
 
+	private float classifyRevision(CSVPrinter resultPrinter) {
+		// This is where an actual classification based on  the revision and
+		// its associated metadata should happen. Instead, we just assign a
+		// score of 0.0, effectively classifying the revision as non-vandalism.
+		Process proc;
+		try {
+			proc = Runtime.getRuntime().exec("java -jar ~/programs/featureextractor.jar --revisiontags ~/data/metadata ~/data/revision ~/data/feature");
+			proc.waitFor();
+			
+			RandomAccessFile pipe = new RandomAccessFile(
+			        "~/data/notify", "rw");
+			 
+			    String req = "datahasarrived";
+			    // Write request to the pipe
+			    pipe.write(req.getBytes());
+			 
+			    // Read response from pipe
+			    String res = pipe.readLine();
+			    if(res != null && !res.equals("")){
+
+			    	BufferedReader br = new BufferedReader(new FileReader("~/data/result"));
+			    	try {
+
+			    	    String line = br.readLine();
+			    	    while (line != null) {
+			    	    	String[] content = line.split(" ");
+			    	    	sendClassificationResult(Long.parseLong(content[0]), Float.valueOf(content[1]), resultPrinter);
+			    	        line = br.readLine();
+			    	    }
+			    	    
+			    	} finally {
+			    	    br.close();
+			    	}
+			    }
+			    // Close the pipe
+			    pipe.close();
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+		return 0.0f;
+	}
+
+	private void sendClassificationResult(
+		long revisionId, float classificationScore, CSVPrinter resultPrinter
+	) {
+		try {
+			resultPrinter.print(revisionId);
+			resultPrinter.print(classificationScore);
+			resultPrinter.println();
+			resultPrinter.flush();
+		}
+		catch (IOException e) {
+			LOG.error("", e);
+		}
+	}
 	private MwDumpFileProcessor createRevisionProcessor(
 		CSVPrinter resultPrinter, BlockingQueue<CSVRecord> metadataQueue
 	) {
@@ -130,8 +200,8 @@ public class Client {
 	}
 
 	private Thread createDemultiplexerThread(
-		DataInputStream dataStream,	PipedOutputStream revisionOutputStream,
-		BlockingQueue<CSVRecord> metadataQueue
+		DataInputStream dataStream,	OutputStream revisionOutputStream,
+		OutputStream metadataQueue
 	) {
 		Demultiplexer d = new Demultiplexer(
 				dataStream, metadataQueue, revisionOutputStream);
